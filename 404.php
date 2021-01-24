59bb970d1d62ef1d5bc855ca6ffa292ec6953f38
Christophe Le Goff <72270681+chrisalexlegoff@users.noreply.github.com>
2021-01-24 16:42:28 +0100

    initial commit

---
+++ 404.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Charm">
+  	<link rel="canonical" href="https://christophe-le-goff.com/404.php">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/logo.css">
+    <link rel="stylesheet" href="css/about.css">
+    <link rel="stylesheet" href="css/social-link.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+    <link rel="manifest" href="site.webmanifest">
+    <meta name="description" content="Portfolio de Christophe Le Goff, un touche a tout passionné. Freelance, je travaille pour vous.">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@chrisalexlegoff">
+    <meta name="twitter:title" content="Christophe Le Goff — Développeur Freelance">
+    <meta name="twitter:description" content="Portfolio de Christophe Le Goff, un touche a tout passionné. Freelance, je travaille pour vous.">
+    <meta name="twitter:image" content="https://www.christophe-le-goff.com/img/1200-630.png">
+    <meta name="twitter:creator" content="@chrisalexlegoff">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://www.christophe-le-goff.com">
+    <meta property="og:title" content="Aristide Benoist — Freelance Developer">
+    <meta property="og:description" content="Portfolio de Christophe Le Goff, un touche a tout passionné. Freelance, je travaille pour vous.">
+    <meta property="og:image" content="https://www.christophe-le-goff.com/img/1200-630.png">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
+
+    <title>Christophe Le Goff | Portfolio</title>
+</head>
+
+<body>
+
+    <div class="container">
+
+        <?php require 'logo.php'; ?>
+	  <p>Oups, cette requète ne peut pas aboutir : revenez à l'<a href="https://chrisalexlegoff.com"> accueil</a></p>
+        <?php require 'social-link.php'; ?>
+
+    </div>
+
+</body>
+
+</html>
---
+++ about.php
@@ -0,0 +1,13 @@
+<div class="about">
+    <div class="title">
+        <h1">Bonjour, je suis Christophe.</h1>
+    </div>
+    <div class="text-about">
+        <p>Passionné et friand de nouveautés, je cherche à devenir développeur web Freelance. J'en suis au commencement,
+            reconversion en cours, je vais m'éclater.</p>
+        <blockquote>
+            <p>Si vous voulez voire ma progression, c'est en dessous ou suivre mes aventures, c'est tout en bas.</p>
+						   <footer>—Christophe Le Goff, <cite><a href="https://christophe-le-goff.com/portfolio"> Mon Travail</cite></a></footer>
+        </blockquote>
+    </div>
+</div>
